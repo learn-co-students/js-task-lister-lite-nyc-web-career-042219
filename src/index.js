b6805e6e738366de91cf040ca537e56aeb489b86
Jake <jakelovitz@gmail.com>
2019-06-05 15:09:45 -0400

    Finished JS Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const task = document.querySelector('#new-task-description');
+  const tasks = document.querySelector('#tasks');
+  const createToDo = document.querySelector('#createToDo');
+  const priority = document.querySelector('#priority-select');
+
+  createToDo.addEventListener('click', function (event) {
+    event.preventDefault()
+    const line = document.createElement('li')
+    line.innerText = task.value
+    line.className = priority.value
+    // debugger
+    const button = document.createElement("button");
+    button.innerText = 'x'
+    tasks.appendChild(line);
+    line.appendChild(button);
+    form.reset();
+    button.addEventListener('click', function (event) {
+      line.remove()
+    })
+  });
 });
